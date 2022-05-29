0e8448e3e9bb64c0058d4a98de583d5db0a56cb7
Alexman604 <102416562+Alexman604@users.noreply.github.com>
2022-05-29 21:20:48 +0300

    RevertCase added
    Method test html connected to revert case.
    Add 2 options to revertCase.js

--- revertCase.js
+++ revertCase.js
@@ -1,16 +1,27 @@
+// 1 - string loop
 const revertCase = (string = '') => {
   let revertedString = '';
   for (const char of string) {
-    console.log(char);
     if (char.toUpperCase() === char) {
       (revertedString += char.toLowerCase());
     }
     else {
       (revertedString += char.toUpperCase());
-    }
-    
-    //console.log(revertedString);
+    }    
   }
-
+  console.log(revertedString);
   return revertedString;
 };
+
+// 2 - for loop
+const revertCase1 = (string = '') => {
+      let newStr = '';
+    for (let i = 0; i < string.length; i++ ) {
+      if (string[i].toLowerCase() === string[i]) {
+       newStr += string[i].toUpperCase();}
+       else {newStr += string[i].toLowerCase();}
+   }
+   console.log(newStr); 
+   return newStr;
+}
+
